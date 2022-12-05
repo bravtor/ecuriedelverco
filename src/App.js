e9e7f3a53017deab6c9138bbba277210be812215
Mac book pro Johann <macbookprojohann@MacBook-Pro-de-Mac.local>
2022-12-05 06:21:41 +0100

    encore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import React from 'react';
-import img5 from "./Img/img500.jpg"
+
 import './App.css';
-import Header from './module/header';
-import Menu from './module/menu';
-import Prestations from './module/prestations';
+
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(${img5})`, backgroundSize: "cover", width: "auto", height: "100vh"}}>
+    <div>
    <p>ceci est un test pour verifir si cela fonctionne bien mais j espere que oui</p>
   </div>
   );
