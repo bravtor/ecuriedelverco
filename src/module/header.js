c22f7ed1442614614eecf7ac67c3a5c4b1bdad98
Mac book pro Johann <macbookprojohann@MacBook-Pro-de-Mac.local>
2022-12-05 01:20:12 +0100

    first commit

---
+++ src/module/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1 className="title">votre ecurie à Corneilla del vercol</h1>
+      <h2 className="title">votre ecurie à Corneilla del vercol</h2>
+      <h2 className="titleA">votre ecurie à Corneilla del vercol</h2>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/module/prestations.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Prestations() {
+  return (
+    <div className="Presta">
+      <button className="ButtonPres">pres A</button>
+      <button className="ButtonPres">pres B</button>
+      <button className="ButtonPres">pres C</button>
+    </div>
+  );
+}
+
+export default Prestations;
